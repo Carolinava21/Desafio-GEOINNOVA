d791e8e2f757f4c12b2a145cab7321b5eb55e96d
Carolina <caroonava21@gmail.com>
2024-07-08 16:47:22 -0400

    2nd division line

--- src/components/HeadingSelect.tsx
+++ src/components/HeadingSelect.tsx
@@ -3,6 +3,7 @@ import "/src/components/headingSelect.css";
 
 export const HeadingSelect: React.FC = () => {
   return (
+    <>
     <div className="select">
       <h3>Heading 2</h3>
       <ul className="list-group">
@@ -11,34 +12,41 @@ export const HeadingSelect: React.FC = () => {
           Option 1
         </li>
         <li>
-          <input type="checkbox" className="checkbox"defaultChecked={true} />
+          <input type="checkbox" className="checkbox" defaultChecked={true} />
           Option 2
         </li>
         <li>
-          <input type="checkbox" className="checkbox"defaultChecked={true} />
+          <input type="checkbox" className="checkbox" defaultChecked={true} />
           Option 3
         </li>
         <li>
-          <input type="checkbox" className="checkbox"defaultChecked={true} />
+          <input type="checkbox" className="checkbox" defaultChecked={true} />
           Option 4
         </li>
         <li>
-          <input type="checkbox"className="checkbox" defaultChecked={true} />
+          <input type="checkbox" className="checkbox" defaultChecked={true} />
           Option 5
         </li>
         <li>
-          <input type="checkbox" className="checkbox"defaultChecked={true} />
+          <input type="checkbox" className="checkbox" defaultChecked={true} />
           Option 6
         </li>
         <li>
-          <input type="checkbox" className="checkbox"defaultChecked={true} id="check" />
+          <input
+            type="checkbox"
+            className="checkbox"
+            defaultChecked={true}
+            id="check"
+          />
           Option 7
         </li>
         <li>
-          <input type="checkbox" className ="check" defaultChecked={false} />
+          <input type="checkbox" className="check" defaultChecked={false} />
           Option 8
         </li>
       </ul>
     </div>
+   
+    </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { PrototypeApp } from "./PrototypeApp";
-import { Menu, } from "./components/Menu";
-import { Paragraph } from './components/Paragraph';
+import { Menu } from "./components/Menu";
+import { Paragraph } from "./components/Paragraph";
 import { HeadingSelect } from "./components/HeadingSelect";
 import "./index.css";
 
@@ -10,9 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <PrototypeApp />
     <Menu />
-    <Paragraph/>
-    <HeadingSelect/>
-   
-   
+    <Paragraph />
+    <HeadingSelect />
   </React.StrictMode>
 );
