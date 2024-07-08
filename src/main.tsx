a8d997a1641e3133765c34e1bf2af9a807cf2b7a
Carolina <caroonava21@gmail.com>
2024-07-08 16:26:38 -0400

    checkbox-selection

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { PrototypeApp } from "./PrototypeApp";
 import { Menu, } from "./components/Menu";
 import { Paragraph } from './components/Paragraph';
+import { HeadingSelect } from "./components/HeadingSelect";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <PrototypeApp />
-    <Paragraph/>
     <Menu />
+    <Paragraph/>
+    <HeadingSelect/>
+   
    
   </React.StrictMode>
 );
